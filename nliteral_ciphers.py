726693b5185905a8e481dee0860740da6e467167
Eric Yanowitz <ericyanowitz00@gmail.com>
2025-09-26 21:05:38 -0400

    write triliteral cipher functions, update some other functions

--- nliteral_ciphers.py
+++ nliteral_ciphers.py
@@ -31,7 +31,6 @@ def biliteral_encoder(text = "", code = "", capitals = False, reverse = False, g
         for i in code:
             if not i.isalpha():
                 code.remove(i)
-    print(code)
     for i in text:
         if grammar == False and not i.isalpha():
             continue
@@ -46,3 +45,110 @@ def biliteral_encoder(text = "", code = "", capitals = False, reverse = False, g
             else:
                 biliteral_string += 'a' 
     return biliteral_string
+
+
+def triliteral_encoder(text = "", codeA = "", codeB = "", capitals = False, grammar = False, allcodes = False):
+    triliteral_string = ""
+    text = text.replace(' ', '')
+    codeA = codeA.replace(' ', '')
+    codeB = codeB.replace(' ', '')
+    if capitals == False:
+        text = text.lower()
+        codeA = codeA.lower()
+        codeB = codeB.lower()
+    if grammar == False:
+        text_temp = ""
+        codeA_temp = ""
+        codeB_temp = ""
+        for i in text:
+            if i.isalpha() or i.isdigit():
+                text_temp += i
+        for i in codeA:
+            if i.isalpha() or i.isdigit():
+                codeA_temp += i
+        for i in codeB:
+            if i.isalpha() or i.isdigit():
+                codeB_temp += i
+        text = text_temp
+        codeA = codeA_temp
+        codeB = codeB_temp
+    codeA = set(codeA)
+    codeB = set(codeB)
+    for i in text:
+        if i in codeA:
+            triliteral_string += 'a'
+        elif i in codeB:
+            triliteral_string += 'b'
+        else: 
+            triliteral_string += 'c'
+    if allcodes == True:
+        t1 = triliteral_string
+        t2 = ""
+        t3 = ""
+        t4 = ""
+        t5 = ""
+        t6 = ""
+        for i in t1:
+            if i == 'a':
+                t2 += 'a'
+                t3 += 'b'
+                t4 += 'b'
+                t5 += 'c'
+                t6 += 'c'
+            elif i == 'b':
+                t2 += 'c'
+                t3 += 'a'
+                t4 += 'c'
+                t5 += 'a'
+                t6 += 'b'
+            elif i == 'c':
+                t2 += 'b'
+                t3 += 'c'
+                t4 += 'a'
+                t5 += 'b'
+                t6 += 'a'
+        triliteral_strings = [t1, t2, t3, t4, t5, t6]
+        return triliteral_strings
+    else:
+        return triliteral_string
+
+
+def interpret_triliteral(triliteral, latin = True, both = False):
+    plaintext = ""
+    d1 = {"aaa":"A","aab":"B","aac":"C","aba":"D","abb":"E","abc":"F","aca":"G","acb":"H","acc":"I","baa":"K","bab":"L","bac":"M","bba":"N","bbb":"O","bbc":"P","bca":"Q","bcb":"R","bcc":"S","caa":"T","cab":"U","cac":"W","cba":"X","cbb":"Y","cbc":"Z"}
+    d2 = {"aaa":"A","aab":"B","aac":"C","aba":"D","abb":"E","abc":"F","aca":"G","acb":"H","acc":"I","baa":"J","bab":"K","bac":"L","bba":"M","bbb":"N","bbc":"O","bca":"P","bcb":"Q","bcc":"R","caa":"S","cab":"T","cac":"U","cba":"V","cbb":"W","cbc":"X","cca":"Y","ccb":"Z"}
+    d = {}
+    if both == True:
+        return interpret_triliteral(triliteral, latin = True, both = False) + interpret_triliteral(triliteral, latin = False, both = False)
+    elif latin == True:
+        d = d1
+    else:
+        d = d2
+    
+    NA_count = 0
+    if type(triliteral) == str:
+        for i in range(0, len(triliteral), 3):
+            j = triliteral[i:i+3]
+            if j in d:
+                plaintext += d[j]
+            else:
+                NA_count += 1
+        plaintext += '\n'
+        print(f'NA count: {NA_count}')
+    elif type(triliteral) == list:
+        print('NA count: ', end ='')
+        for i in range(len(triliteral)):
+            triliteral_l = triliteral[i]
+            plaintext += f"{i+1}: "
+            NA_count = 0
+            for i in range(0, len(triliteral_l), 3):
+                j = triliteral_l[i:i+3]
+                if j in d:
+                    plaintext += d[j]
+                else:
+                    NA_count += 1
+            plaintext += '\n'
+            print(f'{NA_count}, ', end = '')
+        print()
+    return plaintext
+    
--- script.py
+++ script.py
@@ -3,7 +3,22 @@ import text_functions as te
 import substitution_ciphers as sc
 import nliteral_ciphers as nlc
 
-cryptogram = "PTBDWDAHHITNIAOTADIALTTISNBWFBACTOAIOOHATFIIYHTASWAWOBBTWIBOTTAIWTAATTOOTTAITOALAAANBWMTHAWDAAWHTSTTITGTTWOSOWAFTTMIWTAWWTMSAWIBANITTDWTIWAASAOMRWKFIBSHHTFFFBBIAIWTACATTOPINIATTOAOSYWWBSPTWWABOBITWIHTAITFTWIABSAEWMLWWRTWTFOTTWTWOWFTTTAAATRBAIIAAAAAWWDOTCWTATIAIWBTAWAHWYWFTTCFHUWDASSAATTWTWTTATCOFWIT"
-ct.cipher_analysis(cryptogram, n1 = 55, n2 = 10)
-print(sc.caesar_cipher_ex(cryptogram))
+cryptogram = ""
+
+# n = nlc.triliteral_encoder(cryptogram, codeA = 'tawoihfbs', codeB = 'cfgjklmnpqyz', allcodes = True)
+# print(nlc.interpret_triliteral(n))
+n = nlc.biliteral_encoder(cryptogram, code = 'aeiou')
+n2 = nlc.biliteral_encoder(cryptogram, code = 'aeiou', reverse = True)
+print(nlc.interpret_biliteral(n, both = True))
+print(nlc.interpret_biliteral(n2, both = True))
+# d = {'total': 0}
+# for key, value in ct.digraphs(cryptogram, all = True).items():
+#     if key[0] == key[1]:
+#         d['total'] += value
+#         d[key] = value
+# print(d)
+# ct.cipher_analysis(cryptogram)
+# print(ct.letter_count(cryptogram))
+
+
 
