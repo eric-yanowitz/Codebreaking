b218458ac4846cebb916e5b9729c1aab54236c21
Eric Yanowitz <ericyanowitz00@gmail.com>
2025-12-23 13:20:39 -0500

    delete extra text files

--- nliteral_ciphers.py
+++ nliteral_ciphers.py
@@ -1,17 +1,11 @@
-def interpret_biliteral(biliteral, latin = True, both = False, danish = False):
+def interpret_biliteral(biliteral, latin = True, both = False):
     plaintext = ""
     if latin == True:
-        d = {"aaaaa": "A", "aaaab": "B", "aaaba": "C","aaabb": "D", "aabaa": "E", "aabab": "F", "aabba": "G", "aabbb": "H", "abaaa": "I", "abaab": "K", "ababa": "L", "ababb": "M","abbaa": "N", "abbab": "O", "abbba": "P", "abbbb": "Q", "baaaa": "R", "baaab": "S", "baaba": "T", "baabb": "U", "babaa": "W","babab": "X", "babba": "Y", "babbb": "Z"}
-        if danish == True:
-            d['bbaaa'] = "Æ"
-            d['bbaab'] = "Ø"        
+        d = {"aaaaa": "A", "aaaab": "B", "aaaba": "C","aaabb": "D", "aabaa": "E", "aabab": "F", "aabba": "G", "aabbb": "H", "abaaa": "I", "abaab": "K", "ababa": "L", "ababb": "M","abbaa": "N", "abbab": "O", "abbba": "P", "abbbb": "Q", "baaaa": "R", "baaab": "S", "baaba": "T", "baabb": "U", "babaa": "W","babab": "X", "babba": "Y", "babbb": "Z"}     
     else:
         d = {"aaaaa": "A", "aaaab": "B", "aaaba": "C","aaabb": "D", "aabaa": "E", "aabab": "F", "aabba": "G", "aabbb": "H", "abaaa": "I", "abaab": "J", "ababa": "K", "ababb": "L","abbaa": "M", "abbab": "N", "abbba": "O", "abbbb": "P", "baaaa": "Q", "baaab": "R", "baaba": "S", "baabb": "T", "babaa": "U","babab": "V", "babba": "W", "babbb": "X", "bbaaa": "Y", "bbaab": "Z"}
-        if danish == True:
-            d['bbaba'] = "Æ"
-            d['bbabb'] = "Ø"
     if both == True:
-        return interpret_biliteral(biliteral, latin = True, danish = danish) + '\n' + interpret_biliteral(biliteral, latin = False, danish = danish)
+        return interpret_biliteral(biliteral, latin = True) + '\n' + interpret_biliteral(biliteral, latin = False)
     for i in range(0, len(biliteral), 5):
         j = biliteral[i:i+5]
         if j in d:
--- script.py
+++ script.py
@@ -3,14 +3,14 @@ import text_functions as te
 import substitution_ciphers as sc
 import nliteral_ciphers as nlc
 
-cryptogram = ""
+cryptogram = "to see clearly is a beautiful X"
 
 # n = nlc.triliteral_encoder(cryptogram, codeA = 'tawoihfbs', codeB = 'cfgjklmnpqyz', allcodes = True)
 # print(nlc.interpret_triliteral(n))
-n = nlc.biliteral_encoder(cryptogram, code = 'aeiou')
-n2 = nlc.biliteral_encoder(cryptogram, code = 'aeiou', reverse = True)
-print(nlc.interpret_biliteral(n, both = True))
-print(nlc.interpret_biliteral(n2, both = True))
+# n = nlc.biliteral_encoder(cryptogram, code = 'aeiou')
+# n2 = nlc.biliteral_encoder(cryptogram, code = 'aeiou', reverse = True)
+# print(nlc.interpret_biliteral(n, both = True))
+# print(nlc.interpret_biliteral(n2, both = True))
 # d = {'total': 0}
 # for key, value in ct.digraphs(cryptogram, all = True).items():
 #     if key[0] == key[1]:
@@ -19,6 +19,6 @@ print(nlc.interpret_biliteral(n2, both = True))
 # print(d)
 # ct.cipher_analysis(cryptogram)
 # print(ct.letter_count(cryptogram))
-
-
+n = nlc.biliteral_encoder(cryptogram, code = 'toaclrib')
+print(nlc.interpret_biliteral(n, both = True))
 
